462e212fbb5b4a4b4197c63baff453f0f899398f
GitFEliS <115923527+GitFEliS@users.noreply.github.com>
2023-12-11 13:01:38 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='hw_02_1',
+    version='1.0',
+    description='A useful module',
+    author='Man Foo',
+    author_email='foomail@foo.example',
+    packages=['hw_02_1'],  # same as name
+    # external packages as dependencies
+    install_requires=[],
+)
